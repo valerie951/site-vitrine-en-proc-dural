5457ad2f2e2df6006e362f5ad559c541d122316a
valerie951 <76158123+valerie951@users.noreply.github.com>
2021-10-18 18:54:42 +0200

    Add files via upload

---
+++ filrouge/config_db/config.php
@@ -0,0 +1,11 @@
+<?php
+// définition de constante pour les identifiants de connexions à la base de données
+
+define('HOST', 'localhost');
+define('LOGIN', 'root');
+define('PASSWORD', '');
+define('DBNAME', 'val');
+
+// importations de la connexion à la base de données
+
+include '../filrouge/config_db/database.php';
